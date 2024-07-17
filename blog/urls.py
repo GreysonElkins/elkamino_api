7941cb6476bf780f99565210b65c89f3c71aa7bf
Greyson Elkins <greysonelkins@gmail.com>
2024-07-16 18:19:31 -0600

    Add application/json as an option to blog endpoint for consumption by other apps

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 app_name="blog"
 urlpatterns = [
-    path("", views.IndexView.as_view(), name="index"),
+    path("", views.index, name="index"),
     path("<int:pk>/", views.EntryView.as_view(), name="entry")
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,23 @@
+from simplejson import dumps
+
+from django.http import HttpResponse
+from django.core.serializers import serialize
 from django.views import generic
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, get_list_or_404
 
 from .models import Entry
+
+def index(request):
+    #TODO: clean up JSON - try `django.forms.models.model_to_dict`, may have to abandon `get_list_or_404?`
+    entries = get_list_or_404(Entry)
+    print(request.headers['content-type'])
+    if request.headers['content-type'] == 'application/json':
+        response_data = {}
+        response_data['data'] = serialize('json', entries)
+        response_data['status'] = 200
+        return HttpResponse(dumps(response_data), content_type="application/json")
+    else:
+        return render(request, "blog/index.html", { "blog_entries": entries }) 
 
 class IndexView(generic.ListView):
     template_name = "blog/index.html"
@@ -13,5 +29,3 @@ class IndexView(generic.ListView):
 class EntryView(generic.DetailView):
     model = Entry
     template_name = "blog/entry.html"
-
-# Create your views here.
